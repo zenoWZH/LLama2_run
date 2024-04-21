a08485e6dad1f7394ba0e0e6bbb43be467a5dc8b
zenoWZH <wangzhouhao94@gmail.com>
2024-04-21 23:36:32 +0000

    auto update for repo

--- dockersetup.sh
+++ dockersetup.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 sudo chown $USER /var/run/docker.sock
 docker pull yival/release:benchmark
+docker remove inference_benchmark
+docker run --gpus all --name inference_benchmark -p 8888:8888 yival/release:benchmark
+docker restart -t 20 inference_benchmark
+docker exec -d inference_benchmark bash cd ./LLama2_run && git pull origin master
