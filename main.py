d7a9c3b9da50a93bb64bc2053f0c54143886177d
zenoWZH <wangzhouhao94@gmail.com>
2024-04-22 02:01:31 -0700

    seperate system process to free GPU Memory

--- main.py
+++ main.py
@@ -21,50 +21,28 @@ def clear_cache():
     if os.path.exists(cache_dir):
         shutil.rmtree(cache_dir)
     os.makedirs(cache_dir, exist_ok=True)
-    print("Cache cleared")
+    print("Cache cleared")   
 
-def retry_finetuning(model, dataset, access_token, batch_size=4):
-    try:
-        print('='*80)
-        print("\n")
-        print(f"Try again with batch_size = {batch_size}")
-        print('='*80)
-        print("\n")
-        finetuner = LLMFinetuner(model, dataset, access_token, batch_size=batch_size)
-        finetuner.train()
-        del finetuner
-        gc.collect()
-    except RuntimeError as err:
-        del finetuner
-        gc.collect()
-        clear_cache()
-        if batch_size<=1:
-            print(err)
-            print("Aborting fine-tuning of this model and dataset")
-        else:
-            retry_finetuning(model, dataset, access_token, batch_size//2)
-        
-        
 default_access_token = ConfigReader("access_token.txt").read_lines_without_comments()[0]
 
 
-if __name__ == "__main__":
-    if len(sys.argv) > 5 or len(sys.argv<4):
-        print("Usage: python main.py <model_name> <dataset_name> <batch_size> <access_token>")
-        sys.exit(1)
-    elif len(sys.argv) >=4:
-        model_name = sys.argv[1]
-        dataset_name = sys.argv[2]
-        batch_size = sys.argv[3]
-        
-        if len(sys.argv) == 4:
-            access_token = default_access_token
-        else:
-            access_token = sys.argv[4]    
+#if __name__ == "__main__":
+if len(sys.argv) > 5 or len(sys.argv<4):
+    print("Usage: python main.py <model_name> <dataset_name> <batch_size> <access_token>")
+    sys.exit(1)
+elif len(sys.argv) >=4:
+    model_name = sys.argv[1]
+    dataset_name = sys.argv[2]
+    batch_size = sys.argv[3]
     
-    finetuner = LLMFinetuner(model_name, dataset_name, access_token, batch_size)
-    finetuner.train()
-    del finetuner
-    gc.collect()
-    sys.exit(0)
+    if len(sys.argv) == 4:
+        access_token = default_access_token
+    else:
+        access_token = sys.argv[4]    
+
+finetuner = LLMFinetuner(model_name, dataset_name, access_token, batch_size)
+finetuner.train()
+del finetuner
+gc.collect()
+sys.exit(0)
                 
