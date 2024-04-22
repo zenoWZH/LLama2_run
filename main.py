f8be7cd86bdb3c953cfe5d2d9c9339bfa9e2a9d0
zenoWZH <wangzhouhao94@gmail.com>
2024-04-22 02:15:47 -0700

    fix bug

--- main.py
+++ main.py
@@ -31,15 +31,15 @@ os.environ["PYTORCH_CUDA_ALLOC_CONF"] = "max_split_size_mb:128"
 warnings.filterwarnings("ignore")
 default_access_token = ConfigReader("access_token.txt").read_lines_without_comments()[0]
 
-if len(sys.argv) > 5 or len(sys.argv<4):
+if len(sys.argv)>5 or len(sys.argv)<4:
     print("Usage: python main.py <model_name> <dataset_name> <batch_size> <access_token>")
     sys.exit(1)
-elif len(sys.argv) >=4:
+elif len(sys.argv)>=4:
     model_name = sys.argv[1]
     dataset_name = sys.argv[2]
     batch_size = sys.argv[3]
     
-    if len(sys.argv) == 4:
+    if len(sys.argv)==4:
         access_token = default_access_token
     else:
         access_token = sys.argv[4]    
