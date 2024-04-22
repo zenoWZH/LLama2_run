140dbcf64f5d9cbbfff8bcf40b94aab844a9e017
zenoWZH <wangzhouhao94@gmail.com>
2024-04-22 02:43:54 -0700

    fix bug

--- run.py
+++ run.py
@@ -13,7 +13,7 @@ def retry_finetuning(model, dataset, batch_size=4):
         print(f"Try again with batch_size = {batch_size}")
         print('='*80)
         print("\n")
-        os.system(f"poetry run python main.py {model} {dataset} {batch_size}")
+        os.system(f"poetry run python main.py {model} {dataset} {str(batch_size)}")
         print("Training Successful!!!")
         torch.cuda.empty_cache()
         time.sleep(5)
@@ -28,7 +28,7 @@ def retry_finetuning(model, dataset, batch_size=4):
 print(__name__)
 if __name__ == "__main__":
     print("RUNNING!!!")
-    print("\n")
+    #print("\n")
     model_reader = ConfigReader("models.txt")
     dataset_reader = ConfigReader("datasets.txt")
     models = model_reader.read_lines_without_comments()
@@ -49,6 +49,6 @@ if __name__ == "__main__":
             else:
                 torch.cuda.empty_cache()
                 retry_finetuning(model, dataset, batch_size//2)
-            
+
             time.sleep(5)
         
