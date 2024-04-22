1e4b077adee5913d4dc7f8dc9c9313bc59b95b77
zenoWZH <wangzhouhao94@gmail.com>
2024-04-21 21:25:01 -0700

    update pull newest benchmark files

--- dockersetup.sh
+++ dockersetup.sh
@@ -3,4 +3,5 @@ sudo chown $USER /var/run/docker.sock
 docker pull yival/release:benchmark
 docker remove inference_benchmark
 docker run --gpus all --name inference_benchmark -p 8888:8888 -d yival/release:benchmark /bin/bash -c "while true; do sleep 1; done" 
+docker exec -d inference_benchmark /bin/bash -c "cd ./LLama2_run && git pull origin master"
 docker exec -it inference_benchmark /bin/bash -c "cd ./LLama2_run && poetry run python main.py"
