8c6302e1efe12ca5c9dc976638920603a37a2089
zenoWZH <wangzhouhao94@gmail.com>
2024-04-29 17:17:51 -0700

    create shard run

---
+++ docker_run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+sudo chown $USER /var/run/docker.sock
+docker pull yival/release:benchmark
+docker stop inference_benchmark
+docker remove inference_benchmark
+docker run --gpus all --name inference_benchmark -p 8888:8888 -d yival/release:benchmark /bin/bash -c "while true; do sleep 1; done" 
+docker exec -it inference_benchmark /bin/bash -c "cd ./LLama2_run && git pull origin master && poetry run python run.py"
